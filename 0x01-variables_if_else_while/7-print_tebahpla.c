bc9dcf6ab34ec6393dd8cf9c19e08f7eaf21c512
jfangwang <qbs1864@gmail.com>
2020-09-17 13:52:37 -0400

    auto commit: fixed file

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Prints all single digit numbers of base 10,
+ * starting from 0 followed by a new line
+ *
+ * Description: Uses a for loop to putchar all numbers
+ * Return: returns 0
+ */
 int main(void)
 {
 	char c;
