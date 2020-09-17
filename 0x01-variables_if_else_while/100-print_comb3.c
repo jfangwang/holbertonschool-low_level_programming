a446f5084a1030d446f2db7a4ebf69be2c74ac8f
jfangwang <qbs1864@gmail.com>
2020-09-17 14:07:19 -0400

    auto commit: fixed file

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 
+/**
+ * main - Prints all single digit numbers of base 10,
+ * starting from 0 followed by a new line
+ *
+ * Description: Uses a for loop to putchar all numbers
+ * Return: returns 0
+ */
 int main(void)
 {
-	for (int a = 0; a < 10; a++)
+	int a;
+	int b;
+
+	for (a = 0; a < 10; a++)
 	{
-		for (int b = a; b < 10; b++)
+		for (b = a; b < 10; b++)
 		{
 			putchar(a % 10 + '0');
 			putchar(b % 10 + '0');
