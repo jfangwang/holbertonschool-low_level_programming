9313376ac79366be53d362766f72cd39a03bbde5
jfangwang <qbs1864@gmail.com>
2020-09-17 14:03:30 -0400

    auto commit: fixed file

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,17 +9,15 @@
  */
 int main(void)
 {
-	for (int a = 0; a < 10; a++)
+	int a;
+
+	for (a = 0; a < 10; a++)
 	{
-		for (int b = 0; b < 10; b++)
-		{
 		putchar(a % 10 + '0');
-		putchar(b % 10 + '0');
 		if (a == 9 && b == 9)
 			break;
 		putchar(',');
 		putchar(' ');
-		}
 	}
 	putchar('\n');
 	return (0);
