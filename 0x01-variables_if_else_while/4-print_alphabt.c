cd5a0ac406cb3681e754b5fdc5c5146018f5d3a5
jfangwang <qbs1864@gmail.com>
2020-09-17 13:32:43 -0400

    auto commit: fixed file

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * Main: Prints the alphabet in lowercase followed by a new line
+ *
+ * Description: Uses a for loop printing each letter with putchar
+ * 
+ * Return: Returns 0
+ */
 int main(void)
 {
 	char c;
