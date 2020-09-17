d7be6366cf1df667d67f5ad195508a958eda2af1
jfangwang <qbs1864@gmail.com>
2020-09-17 13:53:48 -0400

    auto commit: fixed file

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 
+/**
+ * main - Prints all single digit numbers of base 10,
+ * starting from 0 followed by a new line
+ *
+ * Description: Uses a for loop to putchar all numbers
+ * Return: returns 0
+ */
 int main(void)
 {
 	for (int a = 0; a < 10; a++)
-		putchar(a%10 + '0');
+		putchar(a % 10 + '0');
 	for (char c = 'a'; c < 'g'; c++)
 		putchar(c);
 	putchar('\n');
