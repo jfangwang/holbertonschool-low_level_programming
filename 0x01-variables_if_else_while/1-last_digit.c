fb158be3255fbddea52f79f56bcdbb5a79ce06d1
jfangwang <qbs1864@gmail.com>
2020-09-17 12:33:16 -0400

    auto commit: fixed file

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,7 +15,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	int last_digit = n % 10;
 
-	printf("The last digit of %d is %d ", n, last_digit);
+	printf("Last digit of %d is %d ", n, last_digit);
 	if (n > 5)
 		printf("and is greater than 5\n");
 	if (n == 0)
