2005fcbc2bdcf46ad906d2f7cb08267c746d7d06
jfangwang <qbs1864@gmail.com>
2020-09-17 13:20:57 -0400

    auto commit: fixed file

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - tells you if random number is negative or positive
+ *
+ * Description: Uses if else to print if # if neg/pos/zero
+ * Return: returns zero
+ */
 int main(void)
 {
 	char c;
