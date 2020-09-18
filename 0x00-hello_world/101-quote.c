643a679da20f9970a9a455a706745cbf8504f3b0
jfangwang <qbs1864@gmail.com>
2020-09-18 14:16:47 -0400

    asdf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,10 +5,10 @@
  * main - tells you if random number is negative or positive
  *
  * Description: Uses if else to print if # if neg/pos/zero
- * Return: returns zero
+ * Return: returns 1
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 58);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
