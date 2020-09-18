868f037c14c86e895cccee26ccaca9b6f2484458
jfangwang <qbs1864@gmail.com>
2020-09-18 14:12:48 -0400

    asdf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,13 @@
 
 /*
  * main - Prints "and that piece of art is useful - Dora Korpar, 2015-10-19"
+ * 
  * Description: Uses write to print the statement
+ *
  * Return: returns 1
  */
 int main(void)
 {
 	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 58);
-	return 1;
+	return (1);
 }
