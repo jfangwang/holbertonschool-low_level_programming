42045a531ca88d5abc47e4791d27c80b65fb89b8
jfangwang <qbs1864@gmail.com>
2020-09-22 17:57:43 +0000

    asdf

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * print_alphabet_x10 - print alpha 10 times in a row
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int o;
+
+	char i;
+	
+	for (o = 0; o < 10; o++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		putchar('\n');
+	}
+}
