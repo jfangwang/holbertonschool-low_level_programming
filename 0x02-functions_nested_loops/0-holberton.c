d107b16a438e4b6664c3b4fb9e0385ba93159c65
Jonny Wang <qbs1864@gmail.com>
2020-09-22 12:01:54 -0400

    0

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
-
+	int a;
+	char string[32] = "Holberton";
+	while (string[a] != '\0')
+	{
+		putchar(string[a]);
+		a++;
+	}
+	putchar('\n');
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+	char foo[8] = "Testing";
+	printf("%c". strlen(foo));
+	return (0);
+}
