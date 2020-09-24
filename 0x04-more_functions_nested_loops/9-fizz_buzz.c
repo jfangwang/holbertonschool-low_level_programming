e72569f2c3de4b1a216a65a5fb247b3f1ea4c629
jfangwang <qbs1864@gmail.com>
2020-09-24 14:10:11 -0400

    fixed fizz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,14 +12,18 @@ int main(void)
 		if (a % 3 == 0)
 		{
 			if (a % 5 == 0)
-				printf("FizzBuzz ");
+				printf("FizzBuzz");
 			else
-				printf("Fizz ");
+				printf("Fizz");
 		}
 		if (a % 5 == 0 && a % 3 != 0)
-			printf("Buzz ");
+			printf("Buzz");
 		if (a % 5 != 0 && a % 3 != 0)
-			printf("%d ", a);
+			printf("%d", a);
+		if (a == 100)
+			continue;
+		else
+			putchar(' ');
 	}
 	printf("\n");
 }
