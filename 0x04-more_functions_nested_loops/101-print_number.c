4f1b49b5d1a359ccf140d34cdf6605c3a8f3a873
jfangwang <qbs1864@gmail.com>
2020-09-24 18:11:26 -0400

    101

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * print_number - prints an integer
+ * @n: num
+ * Return: 0
+ */
+void print_number(int n)
+{
+	int rev = 0;
+	int rem = 0;
+
+	if (n < 0)
+	{
+		n *= -1;
+		_putchar('-');
+	}
+
+	while (n > 0)
+	{
+		rem = n % 10;
+		rev = rev * 10 + rem;
+		n /= 10;
+	}
+	while (rev > 0)
+	{
+		_putchar((rev % 10) + '0');
+		rev /= 10;
+	}
+}
