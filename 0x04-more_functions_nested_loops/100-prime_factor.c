fbf15d114b9926d8f95fd85af15d880ca7f882a8
jfangwang <qbs1864@gmail.com>
2020-09-24 16:35:32 -0400

    new logic for 100

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 /**
  * main - prints the larget prime factor of a num
  * Return: 0
@@ -6,23 +7,12 @@
 int main(void)
 {
 	long n = 612852475143;
-	long a, b, fact;
-	/*
-	 * List prime nums
-	 */
-	for (a = 2; a < n; a++)
+	long a;
+
+	for (a = 2; a < sqrt(n); a++)
 	{
-		fact = 0;
-		for (b = 2; b < a; b++)
-		{
-			if (a % b == 0)
-				fact += 1;
-		}
-		if (fact == 0)
-		{
-			if (n % a == 0)
-				n /= a;
-		}
+		while (n % a == 0)
+			n /= a;
 	}
 	printf("%ld\n", n);
 	return (0);
