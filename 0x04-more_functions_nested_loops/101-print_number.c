bad5b787488a048e26d481911d10a5b162827c97
Jonny Wang <qbs1864@gmail.com>
2020-09-24 18:41:16 -0400

    101 done

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,6 +9,8 @@ void print_number(int n)
 	int rev = 0;
 	int rem = 0;
 
+	if (n == 0)
+		_putchar('0');
 	if (n < 0)
 	{
 		n *= -1;
@@ -26,6 +28,4 @@ void print_number(int n)
 		_putchar((rev % 10) + '0');
 		rev /= 10;
 	}
-	if (n ==0)
-		_putchar('0');
 }
