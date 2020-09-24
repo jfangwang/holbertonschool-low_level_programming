7b117f266e2b0a91b85a9e2f8d766f8629dc5cac
Jonny Wang <qbs1864@gmail.com>
2020-09-24 18:55:56 -0400

    101 done

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,7 +2,7 @@
 /**
  * print_number - prints an integer
  * @n: num
- * Return: 0
+ * Return: num
  */
 void print_number(int n)
 {
@@ -14,17 +14,20 @@ void print_number(int n)
 		n *= -1;
 		_putchar('-');
 	}
-	if (n == 0)
+	if (n > 0)
+	{	
+		while (n > 0)
+		{
+			rem = n % 10;
+			rev = rev * 10 + rem;
+			n /= 10;
+		}
+		while (rev > 0)
+		{
+			_putchar((rev % 10) + '0');
+			rev /= 10;
+		}
+	}
+	else
 		_putchar(n + '0');
-	while (n > 0)
-	{
-		rem = n % 10;
-		rev = rev * 10 + rem;
-		n /= 10;
-	}
-	while (rev > 0)
-	{
-		_putchar((rev % 10) + '0');
-		rev /= 10;
-	}
 }
