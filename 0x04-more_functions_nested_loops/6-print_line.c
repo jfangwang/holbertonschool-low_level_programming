16306dcd284c60c77c9cdac9410c142bf0c81039
jfangwang <qbs1864@gmail.com>
2020-09-24 13:05:29 -0400

    6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * print_line - draws a straight line in the term.
+ * @n: input
+ * Result: n or 0
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+		_putchar('\n');
+	int a;
+
+	for (a = 0; a < n; a++)
+	{
+		_putchar('_');
+	}
+}
