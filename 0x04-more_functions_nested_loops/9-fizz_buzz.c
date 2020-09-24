739021c8d1778ad91fa8275933a0b238f39cd306
jfangwang <qbs1864@gmail.com>
2020-09-24 14:04:34 -0400

    9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - runs main
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if (a % 3 == 0)
+		{
+			if (a % 5 == 0)
+				printf("FizzBuzz ");
+			else
+				printf("Fizz ");
+		}
+		if (a % 5 == 0 && a % 3 != 0)
+			printf("Buzz ");
+		if (a % 5 != 0 && a % 3 != 0)
+			printf("%d ", a);
+	}
+	printf("\n");
+}
