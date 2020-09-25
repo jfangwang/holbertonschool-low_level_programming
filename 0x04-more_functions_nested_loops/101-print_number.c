8d45540670539fb25faf100ca384f1565c152025
jfangwang <qbs1864@gmail.com>
2020-09-25 13:41:14 -0400

    fixed 2 errors on 101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,26 +8,37 @@ void print_number(int n)
 {
 	int rev = 0;
 	int rem = 0;
+	int plc = 0;
+	int overflow = 0;
 
 	if (n < 0)
 	{
 		n *= -1;
 		_putchar('-');
 	}
-	if (n > 0)
+	if(n == 0)
 	{
+		_putchar(n + '0');
+		return;
+	}
 		while (n > 0)
 		{
 			rem = n % 10;
-			rev = rev * 10 + rem;
+			if(plc >= 9)
+				overflow = overflow * 10 + rem;
+			else
+				rev = rev * 10 + rem;
 			n /= 10;
+			plc++;
 		}
-		while (rev > 0)
+		while (overflow > 0)
+		{
+			_putchar((overflow % 10) + '0');
+			overflow /= 10;
+		}
+		for (int a = 1; a < plc; a++)
 		{
 			_putchar((rev % 10) + '0');
 			rev /= 10;
 		}
-	}
-	else
-		_putchar(n + '0');
 }
--- 0x04-more_functions_nested_loops/tests/101-main.c
+++ 0x04-more_functions_nested_loops/tests/101-main.c
@@ -7,15 +7,11 @@
  *     */
 int main(void)
 {
-	    print_number(98);
-	        _putchar('\n');
-		    print_number(402);
-		        _putchar('\n');
-			    print_number(1024);
-			        _putchar('\n');
-				    print_number(0);
-				        _putchar('\n');
-					    print_number(-98);
-					        _putchar('\n');
-						    return (0);
+	print_number(100);
+	_putchar('\n');
+	print_number(0);
+	_putchar('\n');
+	print_number(2147483647);
+	_putchar('\n');
+	return (0);
 }
