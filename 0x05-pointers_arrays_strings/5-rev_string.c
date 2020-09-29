d0e4d3fd3e7502968ed90a7586c461ea8cadad8c
jfangwang <qbs1864@gmail.com>
2020-09-29 15:51:29 -0400

    5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,19 +1,26 @@
 #include "holberton.h"
 /**
+ * _strl - returns len of str
+ * @s: string
+ * Return: len of str
+ */
+int _strl(char *s)
+{
+	char *sum = s;
+
+	while (*s)
+		s++;
+	return (s - sum);
+}
+/**
  * rev_string - prints a string in reverse
  * @s: input
  */
 void rev_string(char *s)
 {
-	int a = 0;
+	int a = _strl(s);
 	int b = 0;
 	int temp = 0;
-
-	while (s[a] != '\0')
-	{
-		a++;
-	}
-
 	int c = a - 1;
 
 	for (b = 0; b < (a / 2); b++)
