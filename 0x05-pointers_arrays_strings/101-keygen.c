e3e9974ea1e3d6e22cf142c4fab9d0159e489fde
jfangwang <qbs1864@gmail.com>
2020-09-30 17:59:11 -0400

    deleted

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-	int maxChar = 126; //Max char in ASCII
-	int minChar = 33; //Min char in ASCII
-	int maxBit = 2772; //Exact bitsize length for password
+	int maxChar = 126;
+	int minChar = 33;
+	int maxBit = 2772;
 	
-	srand(time(NULL)); //Setting the seed according to time
-	int randChar = rand() % maxChar; //Outputs a random num
+	srand(time(NULL));
+	int randChar = rand() % maxChar;
 	while (maxBit > 0){
 		while ((randChar < minChar || maxBit - randChar < 33) && maxBit >= 127)
 			randChar = rand() % maxChar;
@@ -22,6 +22,5 @@ int main(void)
 		maxBit -= randChar;
 		randChar = rand() % maxChar;
 	}
-	
 	return (0);
 }	
