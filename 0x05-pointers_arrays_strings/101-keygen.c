9b735a6e915be58b5fcb579ec04cfd40f94835c0
jfangwang <qbs1864@gmail.com>
2020-09-30 17:52:25 -0400

    Re

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,25 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
-int main(void){
-  int max = 126;
-  int current = 0;
-  int randchar = 0;
-
-  srand (time(NULL));
-  while (current != 2772){
-    randchar = rand() % max;
-    while (randchar < 33)
-      randchar = rand() % max;
-    if (randchar + current >= 2739)
-      randchar = rand() % max;
-    if (max < 126 && max >= 33)
-      randchar = max;
-    current = current + randchar;
-    putchar(randchar);
-    if (current >= 2646)
-      max = 2772 - current;
-  }
-  return (0);
+int password()
+{
+	int maxChar = 126; //Max char in ASCII
+	int minChar = 33; //Min char in ASCII
+	int maxBit = 2772; //Exact bitsize length for password
+	
+	srand(time(NULL)); //Setting the seed according to time
+	int randChar = rand() % maxChar; //Outputs a random num
+	//printf("maxBit: %d, randChar: %d\n", maxBit, randChar);
+	while (maxBit > 0){
+		while ((randChar < minChar || maxBit - randChar < 33) && maxBit >= 127)
+			randChar = rand() % maxChar;
+		if (maxBit <= 126)
+			randChar = maxBit;
+		putchar(randChar);
+		maxBit -= randChar;
+		randChar = rand() % maxChar;
+		//printf("\nmaxBit: %d, randChar: %d\n", maxBit, randChar);
+	}
+	
+	//printf("\nEnding\nmaxBit: %d, randChar: %d\n", maxBit, randChar);
+	return (0);
+}	
+int main()
+{
+	password();
+	return(0);
 }
