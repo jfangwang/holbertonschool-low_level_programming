64f4743d451e544514ba0a337c71fab0a12f3cd2
jfangwang <qbs1864@gmail.com>
2020-10-01 17:44:56 -0400

    1

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,19 +1,23 @@
 #include "holberton.h"
 /**
- * *_strncpy - copies a string
- * @*dest: dest
- * @*src: src
- * return: string
+ * _strncpy - copies a string
+ * @dest: dest
+ * @src: src
+ * @n: n
+ * Return: string
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int a = 0;
 
-	while (src[a] && a < n)
+	while (src[a] != '\0')
 	{
-		dest[a] = src[a];
+		if (a < n)
+			dest[a] = src[a];
+		else
+			break;
 		a++;
 	}
-	dest[a + 1] = '\0';
+	//dest[a + 1] = '\0';
 	return (dest);
 }
