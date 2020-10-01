7068e946924547618e2734a327db12d981748b40
jfangwang <qbs1864@gmail.com>
2020-10-01 13:35:08 -0400

    4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * reverse_array - prints a string in reverse
+ * @a: a
+ * @n: n
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int b = 0;
+	int temp = 0;
+
+	for (b = 0; b < (n / 2); b++)
+	{
+		temp = a[b];
+		a[b] = a[n - 1];
+		a[n - 1] = temp;
+		n--;
+	}
+}
