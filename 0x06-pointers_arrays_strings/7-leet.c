c3d19319460d7f7895311ed2565940399de74d8f
jfangwang <qbs1864@gmail.com>
2020-10-01 16:43:11 -0400

    7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,18 +6,21 @@
  */
 char *leet(char *s)
 {
-	int a, b;
-	char leet[10] = {'A', 'a', 'E', 'e', 'O', 'o', 'T', 't', 'L', 'l'};
-	char toleet[10] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+	int a = 0;
+	int b = 0;
+	char leet[] = {'A', 'a', 'E', 'e', 'O', 'o', 'T', 't', 'L', 'l'};
+	char toleet[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
 
 	while (s[a] != '\0')
 	{
 		for (b = 0; b < 10; b++)
+		{
 			if (s[a] == leet[b])
 			{
 				s[a] = toleet[b];
 				break;
 			}
+		}
 		a++;
 	}
 	return (s);
