33883c1be6cca1038ee797633d49ac58613903b1
jfangwang <qbs1864@gmail.com>
2020-10-01 15:51:53 -0400

    7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * leet - encodes a string into 1337
+ * @s: string
+ * Return: s
+ */
+char *leet(char *s)
+{
+	int a, b;
+	char leet[10] = {'A', 'a', 'E', 'e', 'O', 'o', 'T', 't', 'L', 'l'};
+	char toleet[5] = {'4', '3', '0', '7', '1'};
+
+	while (s[a] != '\0')
+	{
+		if ((s[a] >= 'a' && s[a] <= 'z') || (s[a] >= 'A' && s[a] <= 'Z'))
+			for (b = 0; b < 10; b++)
+				if (s[a] == leet[b])
+					s[a] = toleet[b / 2];
+		a++;
+	}
+	return (s);
+}
