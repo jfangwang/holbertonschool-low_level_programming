3870caddd1d615538249877925d8136cf0ea3a30
jfangwang <qbs1864@gmail.com>
2020-10-01 14:47:11 -0400

    6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,12 +7,14 @@
 char *cap_string(char *s)
 {
 	int a = 0;
+	int b = a - 1;
+	int c = a + 1;
 
 	while (s[a])
 	{
-		if (s[a + 1] >= 'a' && s[a + 1] <= 'z')
-			if(s[a - 1] == ' ' || s[a - 1] == '.' || s[a - 1] == '\n' || s[a - 1] == '\t')
-			if (s[a] >= 'a' && s[a] <= 'z')
+		if (s[c] >= 'a' && s[c] <= 'z')
+			if (s[b] == ' ' || s[b] == '.' || s[b] == '\n' || s[b] == '\t')
+				if (s[a] >= 'a' && s[a] <= 'z')
 					s[a] -= 32;
 		a++;
 	}
