d8e962348fe74ace737ad3c9e8109125940740d8
Jonny Wang <qbs1864@gmail.com>
2020-10-01 21:35:34 -0400

    101

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -1,34 +1,32 @@
 #include "holberton.h"
 /**
- * print_number - literally print a number
- * Return: void
- */
+* print_number - literally print a number
+* @n: n
+* Return: void
+*/
 void print_number(int n)
 {
-  int plc = 0;
-  int rev = 0;
+	int plc = 0;
+	int rev = 0;
 
-  if(n < 0)
-  {
-    _putchar('-');
-    n *= -1;
-  }
-  if(n == 0)
-    _putchar(0 + '0');
-  while (n > 0){
-    rev = (rev * 10) + (n % 10);
-    n /= 10;
-    plc++;
-  }
-  while (rev > 0 && plc > 0)
-  {
-    _putchar(rev % 10 + '0');
-    rev /= 10;
-    plc--;
-  }
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+	if (n == 0)
+		_putchar(0 + '0');
+	while (n > 0)
+	{
+		rev = (rev * 10) + (n % 10);
+		n /= 10;
+		plc++;
+	}
+	while (rev > 0 && plc > 0)
+	{
+		_putchar(rev % 10 + '0');
+		rev /= 10;
+		plc--;
+	}
 
 }
-// int main(){
-//   print_number(12345);
-//   return (0);
-// }
