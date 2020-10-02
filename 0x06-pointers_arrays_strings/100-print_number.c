2ee47c3646563084217b072f70685aa26b0c51b4
Jonny Wang <qbs1864@gmail.com>
2020-10-01 23:06:27 -0400

    working 100 but over 40 lines

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -8,25 +8,44 @@ void print_number(int n)
 {
 	int plc = 0;
 	int rev = 0;
+	int rev2 = 0;
 
+	if (n == 0)
+		_putchar(0 + '0');
 	if (n < 0)
 	{
 		_putchar('-');
-		n *= -1;
 	}
-	if (n == 0)
-		_putchar(0 + '0');
-	while (n > 0)
+	while (n < 0)
 	{
-		rev = (rev * 10) + (n % 10);
+		if (plc < 9)
+			rev = (rev * 10) + (-1 * (n % 10));
+		else
+			rev2 = (rev2 * 10) + (-1 * (n % 10));
 		n /= 10;
 		plc++;
 	}
-	while (rev > 0 && plc > 0)
+	while (n > 0)
 	{
-		_putchar(rev % 10 + '0');
-		rev /= 10;
+		if (plc < 9)
+			rev = (rev * 10) + (n % 10);
+		else
+			rev2 = (rev2 * 10) + (n % 10);
+		n /= 10;
+		plc++;
+	}
+	while (plc > 0)
+	{
+		if (rev2 > 0)
+		{
+			_putchar(rev2 % 10 + '0');
+			rev2 /= 10;
+		}
+		else
+		{
+			_putchar(rev % 10 + '0');
+			rev /= 10;
+		}
 		plc--;
 	}
-
 }
--- 0x06-pointers_arrays_strings/tests/100-main.c
+++ 0x06-pointers_arrays_strings/tests/100-main.c
@@ -7,15 +7,25 @@
  */
 int main(void)
 {
-    print_number(98);
+    print_number(-2147483648);
     _putchar('\n');
-    print_number(402);
+    print_number(2147483647);
     _putchar('\n');
-    print_number(1024);
+    print_number(1000);
     _putchar('\n');
     print_number(0);
     _putchar('\n');
     print_number(-98);
+    _putchar('\n');
+    print_number(1234567);
+    _putchar('\n');
+    print_number(100001);
+    _putchar('\n');
+    print_number(23);
+    _putchar('\n');
+    print_number(1232);
+    _putchar('\n');
+    print_number(-48);
     _putchar('\n');
     return (0);
 }
