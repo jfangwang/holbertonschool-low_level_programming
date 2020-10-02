11b13bb39acbf6a40b826df68a955a7ac3ae8406
Jonny Wang <qbs1864@gmail.com>
2020-10-01 23:45:55 -0400

    didnt work reverted back to og copy

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,8 +10,8 @@ char *_strcat(char *dest, char *src)
 	int a = 0;
 	int b = 0;
 
-	while (dest[b++]);
-	b -= 1;
+	while (dest[a++])
+		b++;
 	for (a = 0; src[a]; a++)
 		dest[b++] = src[a];
 	return (dest);
