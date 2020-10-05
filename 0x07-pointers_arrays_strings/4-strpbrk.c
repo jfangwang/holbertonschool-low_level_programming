cf1b6ce11204de9c8c1abf74b31cfee0b5bb99b0
jfangwang <qbs1864@gmail.com>
2020-10-05 16:16:16 -0400

    4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * _strpbrk - function that searches a string for any set of bytes
+ * @s: s
+ * @accept: a
+ * Return: pointer to the byte in s matching accept
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int a = 0;
+	int b = 0;
+	while (s[a] != '\0' || accept[a] != '\0')
+	{
+		if (s[a] == accept[a])
+		{
+			while (accept[b] != '\0')
+			{
+				return (s + a - 1);
+			}
+		}
+		a++;
+	}
+	if (b == 0)
+		return ('\0');
+	return (0);
+}
