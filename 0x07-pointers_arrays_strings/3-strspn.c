f0c7346632ddce150612d5df34771d2b3fde87c4
jfangwang <qbs1864@gmail.com>
2020-10-05 15:59:44 -0400

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * _strspn - Function that gets the len of a prefix string
+ * @s: s
+ * @accept: a
+ * Return: length
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int counter = 0;
+	int a = 0;
+	int b = 0;
+
+	while (accept[a] != '\0')
+	{
+		b = 0;
+		while (s[b] != '\0')
+		{
+			if (s[b] == accept[a])
+			{
+				counter++;
+				break;
+			}
+			b++;
+		}
+		a++;
+	}
+	return (counter);
+}
