ffda07bf50a6d16612086cc323a67c9f56a3fc21
jfangwang <qbs1864@gmail.com>
2020-10-07 12:02:46 -0400

    5-sqrt

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * check - nat. sqr. root. of n
+ * @a: int
+ * @ogNum: ogNum
+ * Return: root of num
+ */
+int check(int a, int ogNum)
+{
+	if (a < 0)
+		return (-1);
+	if (a * a == ogNum)
+		return (a);
+	else
+		return (check(a - 1, ogNum));
+}
+/**
+ * _sqrt_recursion - nat. sqr. root. of n
+ * @n: int
+ * Return: root of num
+ */
+int _sqrt_recursion(int n)
+{
+	int ogNum = n;
+
+	return (check(n, ogNum));
+}
