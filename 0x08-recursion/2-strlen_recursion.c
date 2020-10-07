3212c0fc2a636965fc9981b15c4c8dd1dd59a33c
jfangwang <qbs1864@gmail.com>
2020-10-07 11:15:01 -0400

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/** _strlen_recursion: prints a string
+ * @s: string
+ * Return: string
+ */
+void _strlen_recursion(char *s)
+{
+	int a = 0;
+
+	if (*s != '\0')
+	{
+		a++;
+		a += _strlen_recursion(s + 1);
+	}
+	return (a);
+}
