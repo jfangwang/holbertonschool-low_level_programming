a72f01bd1c301d8489437813d759a93a37f69946
jfangwang <qbs1864@gmail.com>
2020-10-07 11:07:49 -0400

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.c"
+/**
+ * _print_rev_recursion - prints rev
+ * @s: s
+ * Return: s
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != 0)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+	else
+		_putchar('\n');
+}
