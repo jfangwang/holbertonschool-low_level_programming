69003bf5a744a7d0ee5cf969b831b1e05910e8d1
jfangwang <qbs1864@gmail.com>
2020-10-07 13:28:54 -0400

    6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,22 +1,29 @@
 #include "holberton.h"
 /**
- * is_prime_number - prints fact
- * @n: n
- * Return: x^y
+ * check - nat. sqr. root. of n
+ * @a: int
+ * @ogNum: ogNum
+ * Return: root of num
  */
-int check (int ogNum, int current)
+int check(int a, int ogNum)
 {
-	if (ogNum % current == 0 && current < ogNum)
+	if (a < 0)
+		return (-1);
+	if (ogNum % a == 0)
 		return (0);
+	else if (a > 1)
+		return (check(a - 1, ogNum));
 	else
-		return check(ogNum, current + 1);
-	return (1);
+		return (1);
 }
+/**
+ * _sqrt_recursion - nat. sqr. root. of n
+ * @n: int
+ * Return: root of num
+ */
 int is_prime_number(int n)
 {
-	int s = 2;
+	int ogNum = n;
 
-	if (n <= 1)
-		return (0);
-	return check(n, s);
+	return (check(n - 1, ogNum));
 }
