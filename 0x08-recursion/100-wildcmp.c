44a36f9bdd48c4961d649d67d5cd92e62d5830f9
jfangwang <qbs1864@gmail.com>
2020-10-08 18:00:36 -0400

    updated

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -1,21 +1,22 @@
 #include "holberton.h"
+/* Initializing function backtrack() knowing its a valid function */
 int backtrack(char *s1, char *s2, int s1end, int idx2);
 /**
- * length - compares two strings and returns 1 if they are identical
- * @string: string
+ * length - Counts the length of a given string
+ * @string: Given string
  * @count: int count, counts the length
- * Return: 1 or 0
+ * Return: Length of string
  */
 int length(char *string, int count)
 {
-	/*returns length of string*/
+	/*Checks if current index of string is not null*/
 	if (string[count] != '\0')
 		return (length(string, ++count));
 	else
 		return (count);
 }
 /**
- * check_Wild2 - compares two strings and returns 1 if they are identical
+ * check_Wild2 - Compares two strings and returns 1 if they are identical
  * @s1: string 1
  * @s2: string 2 to be tested
  * @idx1: index of s1
@@ -35,26 +36,25 @@ int check_Wild2(char *s1, char *s2, int idx1, int idx2, int s1Len)
 	/*If s2 runs into an asterick */
 	if (s2[idx2] == '*')
 	{
-		/*If char after * is another *, increment idx2*/
+		/*If the char after * is another *, increment idx2*/
 		if (s2[idx2 + 1] == '*')
 			return (check_Wild2(s1, s2, idx1, idx2 + 1, s1Len));
-		/*If char after * is null, it is done iterating and returns 1*/
+		/*If the char after * is null, it is done iterating and returns 1*/
 		if (s2[idx2 + 1] == '\0')
 			return (1);
-		/* If char after * does not match s1, start from end of */
+		/* If the char after * does not match s1, start from end of */
 		/* s1 and go backwards from their until it finds a match. */
-		/* If no match is found going backwards, return 0 (Not Identical). */
+		/* If no match is found going backwards, return 0 (not Identical). */
 		/* Had to create another helper function that does all this for me */
-		/* called backtrack */
+		/* called backtrack() */
 		else
 			return (backtrack(s1, s2, length(s1, 0) - 1, idx2 + 1));
 	}
 	return (0);
 }
 /**
- * backtrack - The purpose of this function is to
- * backtrack to find a certain letter and provide
- * the indexes when the matching letter is found
+ * backtrack - The purpose of this function is to backtrack to find a
+ * certain letter and provide the indexes when the matching letter is found
  * @s1: string 1
  * @s2: string 2 being tested against string 1
  * @s1end: int starting at last index of s1
