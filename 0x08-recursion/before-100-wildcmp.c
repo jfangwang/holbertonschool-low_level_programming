532c4c0368bcffc1d8f680195157dbd9377a021d
jfangwang <qbs1864@gmail.com>
2020-10-08 17:14:57 -0400

    100

---
+++ 0x08-recursion/before-100-wildcmp.c
@@ -0,0 +1,74 @@
+#include "holberton.h"
+/**
+ * wildcmp - compares two strings and returns 1 if they are identical
+ * @s1: string 1
+ * @s2: string 2 to be tested
+ * Return: 1 or 0
+ */
+int length(char *string, int count){
+    //returns length of string
+    if (string[count] != '\0')
+        return (length(string, ++count));
+    else
+        return (count);
+}
+
+int check_Wild2(char *s1, char *s2, int idx1, int idx2, int s1Len){
+    /* s1: String 1
+     * s2: Comparing this string to string 1
+     * idx1:index of s1
+     * idx2: index of s2
+     */
+    //If s1 and s2 both reach null, it ran with no problems returning 1
+    //printf("s1: %s, s2: %s, idx1: %d, idx2: %d, s1Len: %d\n", s1, s2, idx1, idx2, s1Len);
+    if (s2[idx2] == '\0' && s1[idx1] == '\0')
+        return 1;
+    //If they are the same, increment both
+    if (s2[idx2] == s1[idx1])
+        return (check_Wild2(s1, s2, idx1 + 1, idx2 + 1, s1Len));
+
+    //If s2 runs into an *
+    if (s2[idx2] == '*')
+    {
+        //printf("Going into * if statement\n");
+        //If char after * is another *, increment idx2
+        if (s2[idx2 + 1] == '*'){
+            //printf("next idx is *\n");
+            return (check_Wild2(s1, s2, idx1, idx2 + 1, s1Len));
+        }
+            
+        //If char after * is null, it is done iterating and returns 1
+        if (s2[idx2 + 1] == '\0'){
+            //printf("next idx is NULL\n");
+            return (1);
+        }
+        //If char after * matches s1, increment s1 and s2 
+        // if (s2[idx2 + 1] == s1[idx1]){
+        //     printf("next idx is a letter\n");
+        //     return check_Wild2(s1, s2, idx1 + 1, idx2 + 2, s1Len);
+        // }
+        //If char after * does not match s1, start from end of 
+        //s1 and go backwards from their until it finds a match. 
+        //If no match is found going backwards, return 0 (Not Identical)
+        else
+            return backtrack(s1, s2, length(s1, 0) - 1, idx2 + 1);
+    }
+    return (0);
+}
+//Purpose of this function is to backtrack to find a certain letter and provide the indexes of matching letter
+int backtrack (char *s1, char *s2, int s1end, int idx2){
+    //printf("s1end is %d\n",s1end);
+    if(s1end < 0)
+        return 0;
+    if(s1[s1end] != s2[idx2]){
+        return backtrack(s1, s2, s1end - 1, idx2);
+    }
+    else
+        return check_Wild2(s1, s2, s1end + 1, idx2 + 1, length(s1, 0));
+}
+
+int wildcmp(char *s1, char *s2)
+{
+ //check_Wild(s1, s2, 0, 0);   
+ check_Wild2(s1, s2, 0, 0, length(s1, 0));
+}
