79853dbbbf613b85d3523dd3e70f66090d4fe7e9
jfangwang <qbs1864@gmail.com>
2020-10-09 14:43:39 -0400

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - preint out name
+ * @a - args
+ * @b - argv
+ * Return: name
+ */
+int main(int a, char *b[])
+{
+	(void)a;
+	printf("%s\n", b[0]);
+	return (0);
+}	
