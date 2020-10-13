6efc7a237362460a53d451531232d20a47ea86a7
jfangwang <qbs1864@gmail.com>
2020-10-13 14:16:55 -0400

    4-free_grid.c

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+* free_grid -  a
+* @grid:a
+* @height:  a
+* Return: a
+*/
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[index]);
+	free(grid);
+}
+
