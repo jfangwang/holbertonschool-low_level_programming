8021765e5cd66e59b4351b2f5819a7a57a2274f5
jfangwang <qbs1864@gmail.com>
2020-10-14 09:22:45 -0400

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,16 +15,23 @@ int **alloc_grid(int width, int height)
 	int h_ind = 0;
 	int w_ind = 0;
 
+	/* Null if under 0 */
 	if (width <= 0 || height <= 0)
 		return (NULL);
+	/* allocate memory for w * h array */
 	d = malloc(sizeof(int *) * height);
+	/* checks if allocated space is null */
 	if (d == NULL)
 		return (NULL);
+	/* Fun begins, iterates through height */
 	for (h_ind = 0; h_ind < height; h_ind++)
 	{
+		/* Allocate space width at given height */
 		d[h_ind] = malloc(sizeof(int) * width);
+		/* checks if index of given width is null */
 		if (d[h_ind] == NULL)
 		{
+			/* if its null, use free() */
 			while (h_ind >= 0)
 			{
 				free(d[h_ind]);
@@ -38,7 +45,6 @@ int **alloc_grid(int width, int height)
 	{
 		for (w_ind = 0; w_ind < width; w_ind++)
 			d[h_ind][w_ind] = 0;
-	printf("h_ind: %d", h_ind);
 	}
 	return (d);
 }
--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -11,6 +11,29 @@
 
 char *argstostr(int ac, char **av)
 {
+	char *string;
+	int byte;
+	int index;
+	int argument;
+	int size = ac;
+
+	if (av == NULL || ac == NULL)
+		return (NULL);
+	while (argument < ac)
+	{
+		while (av[arg][byte] != '\0')
+			size++;
+		argument++;
+	}
+	string = malloc(sizeof(char) * size + 1);
+
+	index = 0;
 	
+	while (argument < ac)
+	{
+		while (av[arg][byte] != '\0')
+			size++;
+		argument++;
+	}
 }
 
