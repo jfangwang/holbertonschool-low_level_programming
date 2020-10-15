c7a9362bce222c840ed59194c8cc230a98915eeb
jfangwang <qbs1864@gmail.com>
2020-10-15 14:09:21 -0400

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,14 +14,14 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int a;
 	char *pat;
 
-	if (size == 0 || nmemb == 0)
+	if (size <= 0 || nmemb <= 0)
 		return (NULL);
 	pat = malloc(nmemb * size);
 	if (pat == NULL)
-		return (0);
+		return (NULL);
 	while (a != size)
 	{
-		*(pat + (size * a)) = 0;
+		*(pat + a) = 0;
 		a++;
 	}
 	return (pat);
