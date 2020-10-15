388ee9082302d618875e77236a4e8644adb14291
jfangwang <qbs1864@gmail.com>
2020-10-15 14:29:03 -0400

    3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -10,12 +10,15 @@
 
 int *array_range(int min, int max)
 {
-	int *arr, size;
+	int *arr
+	int size;
 
 	if (min > max)
 		return (NULL);
 	size = max - min + 1;
+
 	arr = malloc(sizeof(int) * size);
+
 	if (arr == NULL)
 		return (NULL);
 	int a = 0;
