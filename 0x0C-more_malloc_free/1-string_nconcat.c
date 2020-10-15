de3e177f2cb4ed18d0f5bee82f4baf539b2d98c0
jfangwang <qbs1864@gmail.com>
2020-10-15 13:54:48 -0400

    under

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,16 +11,16 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	/*counter */
 	char *concat;
 	int fredrick1 = 0;
 	int fredrick2 = 0;
+	unsigned int index = 0;
+	unsigned int index2 = 0;
 
 	while (s1[fredrick1] != '\0')
 		fredrick1++;
 	while (s2[fredrick2] != '\0')
 		fredrick2++;
-
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
@@ -30,9 +30,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	concat = malloc(fredrick1 + n + 1);
 	if (concat == NULL)
 		return (NULL);
-	unsigned int index = 0;
-	unsigned int index2 = 0;
-
 	for (index = 0; s1[index] != '\0'; index++, index2++)
 		concat[index2] = s1[index];
 	for (index = 0; index != n; index++, index2++)
@@ -40,4 +37,3 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	concat[index2] = '\0';
 	return (concat);
 }
-
