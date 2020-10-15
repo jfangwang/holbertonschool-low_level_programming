ea52836e388ee4d452e892a138d7f253c86cd882
jfangwang <qbs1864@gmail.com>
2020-10-15 13:50:24 -0400

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 #include "holberton.h"
-
+#include <stdlib.h>
 /**
 * string_nconcat -  a
 * @s1:  a
 * @s2:  a
-* @int n:a
+* @n:a
 * Return: a
 */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	/*counter */
+	char *concat;
 	int fredrick1 = 0;
 	int fredrick2 = 0;
 
@@ -19,22 +20,24 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		fredrick1++;
 	while (s2[fredrick2] != '\0')
 		fredrick2++;
-	
+
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	for (index = 0; s1[index] || s2[index]; index++)
-		l++;
-	willy = malloc(sizeof(char) * l);
-	if (willy == NULL)
+	if ((int) n > fredrick2)
+		n = fredrick2;
+	concat = malloc(fredrick1 + n + 1);
+	if (concat == NULL)
 		return (NULL);
-	for (index = 0; s1[index]; index++)
-		willy[arr2++] = s1[index];
-	for (index = 0; s2[index]; index++)
-		willy[arr2++] = s2[index];
-	return (willy);
-}
+	unsigned int index = 0;
+	unsigned int index2 = 0;
 
+	for (index = 0; s1[index] != '\0'; index++, index2++)
+		concat[index2] = s1[index];
+	for (index = 0; index != n; index++, index2++)
+		concat[index2] = s2[index];
+	concat[index2] = '\0';
+	return (concat);
 }
 
