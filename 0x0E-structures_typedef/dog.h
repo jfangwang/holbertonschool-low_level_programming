bdc302919d4cf61946fee8b7f9628bec20983efe
jfangwang <qbs1864@gmail.com>
2020-10-19 14:11:24 -0400

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+* struct dog -  a
+* @name:  a
+* @age: a 
+* @owner: a
+* Return: a
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
