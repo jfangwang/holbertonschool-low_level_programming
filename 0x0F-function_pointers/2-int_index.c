d14921385fecf8bca5beea49b1b4ceed30125e6a
jfangwang <qbs1864@gmail.com>
2020-10-21 15:32:55 -0400

    2-

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,12 +1,9 @@
-#include "stdio.h"
-#include "stdarg.h"
-#include "string.h"
-#include "stdlib.h"
 #include "function_pointers.h"
 /**
 *int_index - Write a function that searches for an integer.
 *@array: a variable
 *@size: a variable
+*@cmp: a variable
 *Return: 0
 **/
 
@@ -16,7 +13,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (array && cmp && size)
 		for (willy = 0; willy < size; willy++)
+		{
+			cmp(array[willy]);
 			if (cmp(array[willy]))
 				return (willy);
+		}
 	return (-1);
 }
