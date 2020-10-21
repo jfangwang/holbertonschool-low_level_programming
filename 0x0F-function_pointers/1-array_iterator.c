cb739c69134cbf74c8a06bdfe33371ea1615fd34
jfangwang <qbs1864@gmail.com>
2020-10-21 15:20:01 -0400

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -2,7 +2,7 @@
 #include "stdarg.h"
 #include "string.h"
 #include "stdlib.h"
-#include "holberton.h"
+#include "function_pointers.h"
 /**
 *array_iterator - Write a function that executes a function given as a parameter on each element of an array.
 *@array: a variable
@@ -12,5 +12,8 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-
+	unsigned int willy = 0;
+	if (action && array)
+		for (willy = 0; willy < size; willy++)
+			action(array[willy]);
 }
