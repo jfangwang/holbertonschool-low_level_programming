4b34e5f02fac1645efb312307e7912af58a72ed2
jfangwang <qbs1864@gmail.com>
2020-10-29 18:17:11 -0400

    betty compliant and added an edge case for temp

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,6 +1,7 @@
 #include "lists.h"
 /**
-**add_node - Write a function that adds a new node at the beginning of a list_t list.
+**add_node - Write a function that adds a
+*new node at the beginning of a list_t list.
 *@head: a variable
 *@str: a variable
 *Return: 0
@@ -14,11 +15,13 @@ list_t *add_node(list_t **head, const char *str)
 	if (head == NULL)
 		return (NULL);
 	temp = malloc(sizeof(list_t));
+	if (temp == NULL)
+		return (NULL);
 	temp->str = strdup(str);
 	while (str[willy])
 		willy++;
 	temp->len = willy;
 	temp->next = *head;
 	*head = temp;
-	return(temp);
+	return (temp);
 }
