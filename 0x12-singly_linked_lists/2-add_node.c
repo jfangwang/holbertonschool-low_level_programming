609e41c077d74d66f95055e151c0c0b1bd2ec808
jfangwang <qbs1864@gmail.com>
2020-10-29 18:15:42 -0400

    2 is done, willy is gone

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,16 +1,24 @@
-#include "stdio.h"
-#include "stdarg.h"
-#include "string.h"
-#include "stdlib.h"
-#include "holberton.h"
+#include "lists.h"
 /**
 **add_node - Write a function that adds a new node at the beginning of a list_t list.
-*@*head: a variable
-*@char *str: a variable
+*@head: a variable
+*@str: a variable
 *Return: 0
 **/
 
 list_t *add_node(list_t **head, const char *str)
 {
+	list_t *temp;
+	int willy = 0;
 
+	if (head == NULL)
+		return (NULL);
+	temp = malloc(sizeof(list_t));
+	temp->str = strdup(str);
+	while (str[willy])
+		willy++;
+	temp->len = willy;
+	temp->next = *head;
+	*head = temp;
+	return(temp);
 }
