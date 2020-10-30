6ac4b5a18324a76235d58db3f2a82f02c5cc818b
jfangwang <qbs1864@gmail.com>
2020-10-29 23:18:54 -0400

    4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,15 +1,21 @@
-#include "stdio.h"
-#include "stdarg.h"
-#include "string.h"
-#include "stdlib.h"
-#include "holberton.h"
+#include "lists.h"
 /**
 *free_list - Write a function that frees a list_t list.
 *@head: a variable
 *Return: 0
 **/
-
 void free_list(list_t *head)
 {
+	list_t *willy = head;
 
+	if (head == NULL)
+		return;
+	while (head != NULL)
+	{
+		willy = head;
+		head = head->next;
+		free(willy->str);
+		free(willy);
+	}
+	free(head);
 }
