83d4a1faf6653fa034874b9a3cda5b7dce5c9309
Jonny Wang <qbs1864@gmail.com>
2020-11-03 19:08:37 -0500

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,15 +1,26 @@
-#include "stdio.h"
-#include "stdarg.h"
-#include "string.h"
-#include "stdlib.h"
-#include "holberton.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <stdlib.h>
+#include "lists.h"
 /**
 *print_listint - Write a function that prints all the elements of a listint_t list.
-*@listint_t *h: a variable
+*@h: a variable
 *Return: 0
 **/
 
 size_t print_listint(const listint_t *h)
 {
+	/* Willy likes to count */
+	size_t willy = 0;
 
+	if (h == NULL)
+		return (0);
+	while (h != NULL)
+	{
+		printf("%i\n", h->n);
+		h = h->next;
+		willy += 1;
+	}
+	return (willy);
 }
