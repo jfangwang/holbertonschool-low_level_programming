89d79891818d11b82bf5a49794c62dad7915a73e
jfangwang <qbs1864@gmail.com>
2020-11-03 23:38:55 -0500

    9-

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,16 +1,41 @@
-#include "stdio.h"
-#include "stdarg.h"
-#include "string.h"
-#include "stdlib.h"
-#include "holberton.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <stdlib.h>
+#include "lists.h"
 /**
-**insert_nodeint_at_index - Write a function that inserts a new node at a given position.
-*@*head: a variable
-*@int idx: a variable
+**insert_nodeint_at_index - Write a function that returns the nth node of a listint_t linked list.
+*@head: a variable
+*@idx: a variable
+*@n: a variable
 *Return: 0
 **/
-
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
+	listint_t *cur, *willy;
+	unsigned int count = 0;
 
+	if (head == NULL)
+		return (NULL);
+
+	willy = malloc(sizeof(listint_t));
+
+	if (willy == NULL)
+		return (NULL);
+	/* Willy stores the requested node */
+	willy->n = n;
+	willy->next = NULL;
+	/* iterate through the list */
+	cur = *head;
+	for (count = 0; count <= idx; count++)
+	{
+		/*the linked list could be < idx*/
+		if (cur == NULL)
+			return (NULL);
+		else
+			cur = cur->next;
+	}
+	willy->next = cur->next;
+	cur->next = willy;
+	return (willy);
 }
