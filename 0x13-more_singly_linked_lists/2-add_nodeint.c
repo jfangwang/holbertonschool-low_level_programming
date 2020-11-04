618a1c08bedf4850d20e2d9c547d1e81b67ea519
Jonny Wang <qbs1864@gmail.com>
2020-11-03 22:01:43 -0500

    1-listint_len.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,8 +1,8 @@
-#include "stdio.h"
-#include "stdarg.h"
-#include "string.h"
-#include "stdlib.h"
-#include "holberton.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <stdlib.h>
+#include "lists.h"
 /**
 **add_nodeint - Write a function that adds a new node at the beginning of a listint_t list.
 *@*head: a variable
@@ -12,5 +12,16 @@
 
 listint_t *add_nodeint(listint_t **head, const int n)
 {
+	listint_t *willy;
+	listint_t **willysHead = head;
 
+	if (!head)
+		return (NULL);
+
+	willy = malloc(sizeof(listint_t));
+
+	willy->n = n;
+	willy->next = *willysHead;
+	*willysHead = willy;
+	return (willy);
 }
--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,16 +1,37 @@
-#include "stdio.h"
-#include "stdarg.h"
-#include "string.h"
-#include "stdlib.h"
-#include "holberton.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <stdlib.h>
+#include "lists.h"
 /**
-**add_nodeint_end - Write a function that adds a new node at the end of a listint_t list.
-*@*head: a variable
-*@int n: a variable
+**add_nodeint_end - Write a function that adds a new node at the beginning of a listint_t list.
+*@head: a variable
+*@n: a variable
 *Return: 0
 **/
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
+	listint_t *willy;
 
+
+	willy = malloc(sizeof(listint_t));
+	if (!willy)
+		return (NULL);
+
+	willy->n = n;
+
+	if (!*head)
+	{
+		willy->next = NULL;
+		*head = willy;
+		return (*head);
+	}
+
+	while (*head->next != NULL)
+		*head = willysHead->next;
+	
+	*head->next = willy;
+
+	return (willy);
 }
