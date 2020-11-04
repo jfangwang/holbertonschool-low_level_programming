2a735972de2accb56a194f744d77bcc73f9e8c24
jfangwang <qbs1864@gmail.com>
2020-11-03 22:56:08 -0500

    8

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,8 +1,8 @@
-#include "stdio.h"
-#include "stdarg.h"
-#include "string.h"
-#include "stdlib.h"
-#include "holberton.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <stdlib.h>
+#include "lists.h"
 /**
 *sum_listint - Write a function that returns the sum of all the data (n) of a listint_t linked list.
 *@head: a variable
@@ -11,5 +11,15 @@
 
 int sum_listint(listint_t *head)
 {
+	unsigned int count = 0;
+	int sum;
 
+	if (head == NULL)
+		return (0);
+        for (count = 0; head != NULL; count++)
+        {
+		sum += head->n;
+		head = head->next;
+        }
+	return (sum);
 }
