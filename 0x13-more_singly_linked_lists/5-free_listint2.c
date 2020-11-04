9ebe29584bcea69a2424e373627b8f4150c1bf6b
jfangwang <qbs1864@gmail.com>
2020-11-03 22:27:36 -0500

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,15 +1,25 @@
-#include "stdio.h"
-#include "stdarg.h"
-#include "string.h"
-#include "stdlib.h"
-#include "holberton.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <stdlib.h>
+#include "lists.h"
 /**
 *free_listint2 - Write a function that frees a listint_t list.
-*@*head: a variable
+*@head: a variable
 *Return: 0
 **/
 
 void free_listint2(listint_t **head)
 {
+	listint_t *willy = head;
 
+        if (head == NULL)
+                return;
+
+        while (head != NULL)
+        {
+                willy = head;
+                head = head->next;
+                free(willy);
+        }
 }
