beebd692523684892ca2000a63e209845e8ed927
Jonny Wang <qbs1864@gmail.com>
2020-11-05 12:53:49 -0500

    asdf

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,11 +1,11 @@
 #include "holberton.h"
 /**
-*get_bit - Write a function that returns
-*the value of a bit at a given index.
-*@n: a variable
-*@index: a variable
-*Return: 0
-**/
+ *get_bit - Write a function that returns
+ *the value of a bit at a given index.
+ *@n: a variable
+ *@index: a variable
+ *Return: 0
+ */
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int count = 0;
