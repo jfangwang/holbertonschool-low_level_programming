907a96d8969e3363b0e5fa8b8463e92e8f8c57b2
Jonny Wang <qbs1864@gmail.com>
2020-11-05 14:31:00 -0500

    asdf

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,14 +1,11 @@
-#include "stdio.h"
-#include "stdarg.h"
-#include "string.h"
-#include "stdlib.h"
 #include "holberton.h"
 /**
-*clear_bit - Write a function that sets the value of a bit to 0 at a given index.
-*@long int *n: a variable
-*@int index: a variable
-*Return: 0
-**/
+ *clear_bit - Write a function that sets
+ *the value of a bit to 0 at a given index.
+ *@n: a variable
+ *@index: a variable
+ *Return: 0
+ */
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
