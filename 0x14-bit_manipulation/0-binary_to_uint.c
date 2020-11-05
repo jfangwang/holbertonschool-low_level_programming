c0088bb32ee066247e18568f0488285340a42597
Jonny Wang <qbs1864@gmail.com>
2020-11-05 10:07:23 -0500

    0 done

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,15 +1,35 @@
-#include "stdio.h"
-#include "stdarg.h"
-#include "string.h"
-#include "stdlib.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <stdlib.h>
 #include "holberton.h"
 /**
-*int binary_to_uint - Write a function that converts a binary number to an unsigned int.
-*@char *b: a variable
+*binary_to_uint - Write a function that converts 
+*a binary number to an unsigned int.
+*b: a variable
 *Return: 0
 **/
 
 unsigned int binary_to_uint(const char *b)
 {
+	int i = 0;
+	int bit = 1;
+	int sum = 0;
 
+	if (!b)
+		return(0);
+	while (b[i] != '\0')
+		i++;
+	i--;
+	while (i >= 0)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		if (b[i] == '1')
+			sum += bit;
+		bit *= 2;
+		i--;
+	}
+	
+	return(sum);
 }
