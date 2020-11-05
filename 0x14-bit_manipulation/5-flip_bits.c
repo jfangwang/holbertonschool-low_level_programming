89256771f990077747de25ee9a2c96497e6ea0be
Jonny Wang <qbs1864@gmail.com>
2020-11-05 14:45:36 -0500

    5 works and betty

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,8 +1,9 @@
 #include "holberton.h"
 /**
-*int flip_bits - Write a function that returns the number of bits you would need to flip to get from one number to another.
-*@long int n: a variable
-*@long int m: a variable
+*flip_bits - Write a function that returns the number
+*of bits you would need to flip to get from one number to another.
+*@n: a variable
+*@m: a variable
 *Return: 0
 **/
 
