331c38a81ccc304a1110d9896a03fd1a98e3f15a
Jonny Wang <qbs1864@gmail.com>
2020-11-05 13:54:27 -0500

    asdf

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,19 +8,7 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int count = 0;
-	unsigned long int temp = n;
-
-	while (n > 0)
-	{
-		count++;
-		n = n >> 1;
-	}
-	if (index > count)
-		return (-1);
 	if (index >= 64)
 		return (-1);
-	n = temp;
-
 	return (n >> index & 1);
 }
