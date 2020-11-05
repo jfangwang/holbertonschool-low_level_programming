1ac32598d8ffc112387fa0c9ca65cb05b0b0082b
Jonny Wang <qbs1864@gmail.com>
2020-11-05 12:14:41 -0500

    asdf

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,7 +1,3 @@
-#include <stdio.h>
-#include <stdarg.h>
-#include <string.h>
-#include <stdlib.h>
 #include "holberton.h"
 /**
 *get_bit - Write a function that returns
@@ -10,7 +6,6 @@
 *@index: a variable
 *Return: 0
 **/
-
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int count = 0;
