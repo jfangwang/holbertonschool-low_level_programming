cb04be1a09ea696020173ae767b85b60b915ca5c
jfangwang <qbs1864@gmail.com>
2020-11-09 23:57:14 -0500

    2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+*append_text_to_file - creates a file
+*@filename: var
+*@text_content: var
+*Return: 1 OR -1
+*/
+int append_text_to_file(const char *filename, char *text_content)
+{
+	if (!filename)
+		return (-1);
+	if (!text_content)
+		return (-1);
+	return (1);
+}
