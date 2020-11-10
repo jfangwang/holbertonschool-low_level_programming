29ac342fabae287ffe45a26316fd6de01ca021c9
Jonny Wang <qbs1864@gmail.com>
2020-11-10 09:50:37 -0500

    1 fixed

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,9 +23,28 @@ size_t _strlen(char *s)
 */
 int create_file(const char *filename, char *text_content)
 {
-	if (!filename)
+	int closer, file = 0;
+	ssize_t len, written = 0;
+
+	if (!filename | !text_content)
 		return (-1);
-	if (!text_content)
+
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+
+	if (!file)
 		return (-1);
+
+	len = _strlen(text_content);
+
+	written = write(file, text_content, len);
+	
+	if (written == -1)
+		return (-1);
+
+	closer = close(file);
+
+	if (closer == -1)
+		return (-1);
+
 	return (1);
 }
