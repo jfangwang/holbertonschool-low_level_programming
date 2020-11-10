a862ee209aef758047a7db3448ca579f51ad2433
jfangwang <qbs1864@gmail.com>
2020-11-09 23:54:29 -0500

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,4 @@
-#incldue "holberton.h"
+#include "holberton.h"
 
 /**
 *_strlen - finds len
@@ -24,6 +24,8 @@ size_t _strlen(char *s)
 int create_file(const char *filename, char *text_content)
 {
 	if (!filename)
+		return (-1);
+	if (!text_content)
 		return (-1);
 	return (1);
 }
