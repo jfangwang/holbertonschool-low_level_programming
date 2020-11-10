3d139d8a44f0ff625747a25fbe9d18c74ea40284
Jonny Wang <qbs1864@gmail.com>
2020-11-10 09:53:33 -0500

    moved the checker for text content

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,13 +26,15 @@ int create_file(const char *filename, char *text_content)
 	int closer, file = 0;
 	ssize_t len, written = 0;
 
-	if (!filename | !text_content)
+	if (!filename)
 		return (-1);
 
 	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
-	if (!file)
+	if (file == -1)
 		return (-1);
+	if (!text_content)
+		return (1)
 
 	len = _strlen(text_content);
 
