0f5ddb8e151ee9ff364f38da0065107c1766aa8d
Jonny Wang <qbs1864@gmail.com>
2020-11-09 23:19:29 -0500

    0 not done

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+
+ssize_t reading_textfile(const char *filename, size_t letters)
+{
+	char *willy;
+	int fd = 0;
+	int reader = 0;
+	int written = 0;
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	willy = malloc(letters);
+
+	if (!willy)
+		return (0);
+
+	reader = read(fd, willy, letters);
+
+	if (reader == -1)
+	{
+		free(willy);
+		return (0);
+	}
+
+	written = write(STDOUT_FILENO, willy, reader);
+
+	if (reader == -1)
+	{
+		free(willy);
+		return (0);
+	}
+	free(willy);
+	close(fd);
+	return (written);
+
+}
