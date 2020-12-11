dca733c69571aea24d12e92769a4dcc555383ce9
jfangwang <qbs1864@gmail.com>
2020-12-11 20:14:02 +0000

    2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,16 +1,31 @@
-#include "stdio.h"
-#include "stdarg.h"
-#include "string.h"
-#include "stdlib.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <stdlib.h>
 #include "holberton.h"
 /**
-**add_dnodeint - Write a function that adds a new node at the beginning of a dlistint_t list.
-*@*head: a variable
-*@int n: a variable
-*Return: 0
-**/
+ **add_dnodeint - Write a function that adds a new node at the beginning of a dlistint_t list.
+ *@*head: a variable
+ *@int n: a variable
+ *Return: 0
+ **/
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
+    dlistint_t *new, *big_head;
+    int willy = 0;
 
+    if (head == NULL)
+        return (NULL);
+    big_head = *head;
+    new = malloc(sizeof(dlistint_t));
+    if (new == NULL)
+        return (NULL);
+    new->n = n;
+    new->prev = NULL;
+    new->next = *head;
+    if (*head != NULL)
+        big_head->prev = new;
+    *head = new;
+    return (new);
 }
