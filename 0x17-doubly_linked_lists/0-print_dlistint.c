66024dabf4c5337376097a52ba28b7a1cb902357
jfangwang <qbs1864@gmail.com>
2020-12-12 23:40:12 +0000

    0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -4,29 +4,30 @@
 #include <stdlib.h>
 #include "holberton.h"
 /**
-*print_dlistint - Write a function that prints all the elements of a dlistint_t list.
-*@dlistint_t *h: a variable
-*Return: 0
-**/
+ *print_dlistint - Write a function that prints all
+ *the elements of a dlistint_t list.
+ *@h: a variable
+ *Return: 0
+ **/
 
 size_t print_dlistint(const dlistint_t *h)
 {
-    int count = 0;
+	int count = 0;
 
-    while (h)
-    {
-        if (h->n)
-        {
-            printf("%d\n", h->n);
-            count += 1;
-            h = h->next;
-        }
-        else
-        {
-            printf("0\n");
-            h = h->next;
-        }
-    }
-    return (count);
+	while (h)
+	{
+		if (h->n)
+		{
+			printf("%d\n", h->n);
+			count += 1;
+			h = h->next;
+		}
+		else
+		{
+			printf("0\n");
+			h = h->next;
+		}
+	}
+	return (count);
 
 }
