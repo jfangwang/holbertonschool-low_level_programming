c5cd4ff369e829214b7a7fe7e34f3fd0dfe4b617
jfangwang <qbs1864@gmail.com>
2020-12-13 00:05:06 +0000

    changed

--- 0x17-doubly_linked_lists/holberton.h
+++ 0x17-doubly_linked_lists/holberton.h
@@ -1,5 +1,5 @@
 #ifndef HOLBERTON_H
-#define HOLBERTON_Hsize_t print_dlistint(const dlistint_t *h);
+#define HOLBERTON_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -21,6 +21,7 @@ typedef struct dlistint_s
     struct dlistint_s *next;
 } dlistint_t;
 
+size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
