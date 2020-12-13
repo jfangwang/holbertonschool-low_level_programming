f73c9fa7c2ebfe5c5e5af8c196bfdf977f11c289
jfangwang <qbs1864@gmail.com>
2020-12-13 00:13:27 +0000

    betty

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,20 +1,21 @@
 #include "lists.h"
 /**
-*dlistint_len - Write a function that returns the number of elements in a linked dlistint_t list.
-*@dlistint_t *h: a variable
-*Return: 0
-**/
+ *dlistint_len - Write a function that returns the number of elements
+ *in a linked dlistint_t list.
+ *@dlistint_t *h: a variable
+ *Return: 0
+ **/
 
 size_t dlistint_len(const dlistint_t *h)
 {
-    size_t count = 0;
+	size_t count = 0;
 
-    if (!h)
-        return (count);
-    while (h != NULL)
-    {
-        count += 1;
-        h = h->next;
-    }
-    return (count);
+	if (!h)
+		return (count);
+	while (h != NULL)
+	{
+		count += 1;
+		h = h->next;
+	}
+	return (count);
 }
