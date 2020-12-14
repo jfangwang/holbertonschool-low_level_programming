d41ccd7e7b85e1110508b2d93d149355d3db9c1d
jfangwang <qbs1864@gmail.com>
2020-12-14 02:07:00 +0000

    4betty

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,21 +1,21 @@
 #include "lists.h"
 /**
-*free_dlistint - Write a function that frees a dlistint_t list.
-*@head: a variable
-*Return: 0
-**/
+ *free_dlistint - Write a function that frees a dlistint_t list.
+ *@head: a variable
+ *Return: 0
+ **/
 
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *willy = head;
 
-        if (head == NULL)
-                return;
-        while (head != NULL)
-        {
-                willy = head;
-                head = head->next;
-                free(willy);
-        }
-        free(head);
+	if (head == NULL)
+		return;
+	while (head != NULL)
+	{
+		willy = head;
+		head = head->next;
+		free(willy);
+	}
+	free(head);
 }
