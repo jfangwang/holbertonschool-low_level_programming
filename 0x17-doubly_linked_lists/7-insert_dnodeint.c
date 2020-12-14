6a4198ebf4c8f4ec39d45efafb4f62a2cf13a695
jfangwang <qbs1864@gmail.com>
2020-12-14 02:22:53 +0000

    7done

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,47 @@
+#include "lists.h"
+/**
+ **insert_dnodeint_at_index - Write a function
+ *that inserts a new node at a given position.
+ *@h: a variable
+ *@idx: a variable
+ *@n: a value
+ *Return: 0
+ **/
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *cur, *willy;
+	unsigned int count = 0;
+
+	if (h == NULL)
+		return (NULL);
+
+	willy = malloc(sizeof(dlistint_t));
+
+	if (willy == NULL)
+		return (NULL);
+	/* Willy stores the requested node */
+	willy->n = n;
+	willy->next = NULL;
+	cur = *h;
+	if (idx == 0)
+	{
+		cur->next = *h;
+		*h = willy;
+		cur->prev = NULL;
+		return (*h);
+	}
+	/* iterate through the list */
+	cur = *h;
+	for (count = 0; count < idx - 1; count++)
+	{
+		/*the linked list could be < idx*/
+		if (cur == NULL)
+			return (NULL);
+		cur = cur->next;
+	}
+	willy->next = cur->next;
+	cur->next = willy;
+	willy->prev = cur->prev;
+	return (willy);
+}
