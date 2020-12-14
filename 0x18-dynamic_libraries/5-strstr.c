cad1f5270c38609e61ea818d5c7029b624f9e2af
jfangwang <qbs1864@gmail.com>
2020-12-14 13:22:32 -0500

    init

---
+++ 0x18-dynamic_libraries/5-strstr.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _strstr - locates a substring
+ * @haystack: a
+ * @needle: n
+ * Return: pointer to the located substring
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int hc, nc, initialhc;
+
+	while (haystack[hc] != '\0')
+	{
+		initialhc = hc;
+		nc = 0;
+		while (needle[nc] == haystack[hc] &&
+			needle[nc] != '\0' && haystack[hc] != '\0')
+		{
+			nc++;
+			hc++;
+		}
+		if (needle[nc] == '\0')
+			return (haystack + initialhc);
+		hc = initialhc + 1;
+	}
+	return (NULL);
+}
