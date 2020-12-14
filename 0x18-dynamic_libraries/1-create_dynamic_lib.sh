ebb9c7f6fcf296396a26c06ef1122ad24dd4072d
jfangwang <qbs1864@gmail.com>
2020-12-14 19:02:35 +0000

    1 changed

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-gcc -fPIC -shared -Wall -Werror -Wextra -pedantic -o "lib${1:-all}.so" *.c
+gcc -fPIC -c *.c
+gcc -shared -Wl,-soname,libtools.so -o libtools.so *.o
 export LD_LIBRARY_PATH=$PWD:$LD_LIBRARY_PATH
