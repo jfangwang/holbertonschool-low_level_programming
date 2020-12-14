caddaa59abce127f8d3028aa59627ebe9ba7cb26
jfangwang <qbs1864@gmail.com>
2020-12-14 02:11:49 +0000

    6done

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,15 +1,22 @@
-#include "stdio.h"
-#include "stdarg.h"
-#include "string.h"
-#include "stdlib.h"
-#include "holberton.h"
+#include "lists.h"
 /**
-*sum_dlistint - Write a function that returns the sum of all the data (n) of a dlistint_t linked list.
-*@head: a variable
-*Return: 0
-**/
+ *sum_dlistint - Write a function that returns the
+ *sum of all the data (n) of a dlistint_t linked list.
+ *@head: a variable
+ *Return: 0
+ **/
 
 int sum_dlistint(dlistint_t *head)
 {
+	unsigned int count = 0;
+	int sum = 0;
 
+	if (head == NULL)
+		return (0);
+	for (count = 0; head != NULL; count++)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
 }
