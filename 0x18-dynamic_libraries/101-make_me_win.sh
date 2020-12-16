fe2c94b582bc513f656dd52c0215b57e81d461e3
jfangwang <qbs1864@gmail.com>
2020-12-16 23:25:59 +0000

    101 file hcange path to tmp

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl https://github.com/jfangwang/holbertonschool-low_level_programming/blob/master/0x18-dynamic_libraries/win.so -o /tmp/win.so
+wget -O /tmp/win.so https://github.com/jfangwang/holbertonschool-low_level_programming/blob/master/0x18-dynamic_libraries/win.so
 export LD_PRELOAD=/tmp/win.so
