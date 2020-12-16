3c001c4e1f10d89407e4254597f82c0581956e73
jfangwang <qbs1864@gmail.com>
2020-12-16 22:57:21 +0000

    101 test

--- 0x18-dynamic_libraries/test101.sh
+++ 0x18-dynamic_libraries/test101.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-export LD_PRELOAD=/home/vagrant/holbertonschool-low_level_programming/0x18-dynamic_libraries/win.so
+curl https://github.com/jfangwang/holbertonschool-low_level_programming/blob/master/0x18-dynamic_libraries/win.so -o /usr/lib/x86_64-linux-gnu/win.so
+export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/win.so
 ./gm 9 8 10 24 75 9
