dd718595c02e86e1c49a380e6868f6de080ae8b9
jfangwang <qbs1864@gmail.com>
2020-12-16 22:41:02 +0000

    removed ./gm on 101

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 curl https://github.com/jfangwang/holbertonschool-low_level_programming/blob/master/0x18-dynamic_libraries/win.so -o win.so
 export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/win.so
-./gm 9 8 10 24 75 9
