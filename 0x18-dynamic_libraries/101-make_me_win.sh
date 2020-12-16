5ce4c18926f655091c2dcaacd67427f6da2b0b12
jfangwang <qbs1864@gmail.com>
2020-12-16 23:19:10 +0000

    101 changed paths

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl https://github.com/jfangwang/holbertonschool-low_level_programming/blob/master/0x18-dynamic_libraries/win.so -o /usr/lib/x86_64-linux-gnu/win.so
-export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/win.so
+curl https://github.com/jfangwang/holbertonschool-low_level_programming/blob/master/0x18-dynamic_libraries/win.so -o /tmp/win.so
+export LD_PRELOAD=/tmp/win.so
