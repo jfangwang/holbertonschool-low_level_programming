bc0be009c2e5f98d9b6022aafc08e77a5bbd498f
jfangwang <qbs1864@gmail.com>
2020-12-16 22:10:00 +0000

    got the combo, need to install script from the server

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
 export LD_PRELOAD=/home/vagrant/holbertonschool-low_level_programming/0x18-dynamic_libraries/win.so
-./gm 1 2 3 4 5 6
--- 0x18-dynamic_libraries/win.c
+++ 0x18-dynamic_libraries/win.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
 // Inject this script to win
 int count = 0;
+int c = 200;
+
 int rand()
 {
-	printf("Count: %d\n", count);
 	count += 1;
-	return (count);
+	printf("Count) %d\n", count);
+	if (count == 1)
+		return (8);
+	else if (count == 2)
+		return (8);
+	else if (count == 3)
+		return (7);
+	else if (count == 4)
+		return (9);
+	else if (count == 5)
+		return (23);
+	else if (count == 6)
+		return (74);
+	else
+		return (0);
 }
