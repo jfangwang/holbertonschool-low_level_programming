b4b01d743d32bc15ab52bc9af7e7ecf570c40bea
jfangwang <qbs1864@gmail.com>
2020-12-18 06:02:08 +0000

    7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,5 +1,25 @@
 #include "lists.h"
 /**
+*create - creates a new node
+*@prev: prev node
+*@next: next node
+*@a: value
+*/
+dlistint_t *create(dlistint_t *prev, dlistint_t *next, unsigned int a)
+{
+	dlistint_t *node;
+
+	node = malloc(sizeof(dlistint_t));
+	if (node == NULL)
+		return NULL;
+	node->next = next;
+	node->prev = prev;
+	node->n = a;
+	if (prev->next != NULL)
+		prev->next = node;
+	return (node);
+}
+/**
  **insert_dnodeint_at_index - Write a function
  *that inserts a new node at a given position.
  *@h: a variable
@@ -7,42 +27,26 @@
  *@n: a value
  *Return: 0
  **/
-
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *cur, *willy;
+	dlistint_t *cur, *new;
 	unsigned int count = 0;
 
 	if (h == NULL)
 		return (NULL);
-
-	willy = malloc(sizeof(dlistint_t));
-
-	if (willy == NULL)
-		return (NULL);
-	/* Willy stores the requested node */
-	willy->n = n;
-	willy->next = NULL;
-	cur = *h;
 	if (idx == 0)
-	{
-		willy->next = *h;
-		*h = willy;
-		willy->prev = NULL;
-		cur->prev = willy;
-		return (willy);
-	}
+		return (add_dnodeint(h, n));
 	/* iterate through the list */
 	cur = *h;
-	for (count = 0; count < idx - 1; count++)
+	for (count = 0; count < idx; count++)
 	{
 		/*the linked list could be < idx*/
 		if (cur == NULL)
 			return (NULL);
 		cur = cur->next;
 	}
-	willy->next = cur->next;
-	cur->next = willy;
-	willy->prev = cur->prev;
-	return (willy);
+	new = create(cur, cur->next, n);
+	cur->prev = new;
+	cur->prev->prev->next = new;
+	return (new);
 }
