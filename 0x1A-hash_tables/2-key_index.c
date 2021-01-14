bd8524cd9752e29724960e5ab9b8fb1c6238e166
jfangwang <qbs1864@gmail.com>
2021-01-14 18:52:55 +0000

    2 is done

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,20 +1,16 @@
-#include "stdio.h"
-#include "stdarg.h"
-#include "string.h"
-#include "stdlib.h"
-#include "holberton.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <stdlib.h>
+#include "hash_tables.h"
 /**
-*long int key_index - Write a function that gives you the index of a key.
+*key_index - Write a function that gives you the index of a key.
 *@unsigned char *key: a variable
 *@long int size: a variable
 *Return: 0
 **/
 
-unsigned long int key_index(const unsigned char *key, unsigned long int size);
-
-
-where key is the key
-and size is the size of the array of the hash table
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-
+	return (hash_djb2(key) % size);
 }
