321ab65a467f7d288695a889ad95e6b89e445d58
jfangwang <qbs1864@gmail.com>
2021-01-15 20:15:41 +0000

    asdf

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -38,6 +38,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		if (strcmp(find->key, key) == 0)
 		{
 			find->value = strdup(value);
+			return (1);
 		}
 	}
 	new->next = ht->array[index];
