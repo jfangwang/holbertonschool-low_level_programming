ea3377f2430ac79fa78a9c775685ce193a70b788
jfangwang <qbs1864@gmail.com>
2021-01-15 16:38:07 +0000

    fixing 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -5,8 +5,7 @@
 #include "hash_tables.h"
 /**
  *hash_table_set - Write a function that adds an element to the hash table.
- *@ht: a variable
- *@key: a variable
+ *@ht: a variable *@key: a variable
  *@value: a variable
  *Return: 0
  **/
@@ -14,11 +13,37 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *new;
+	hash_node_t *find, *new;
 
 	index = key_index((unsigned char *)key, ht->size);
 	if (ht == NULL || key == NULL || value == NULL || strlen(key) == 0)
 		return (0);
+	for (find = ht->array[index]; find != NULL; find = find-> next)
+	{
+		if (strcmp(find->key, key) == 0)
+		{
+			free(find->value);
+			find->value = (char *)value;
+			if (find->value == NULL)
+				return (0);
+			return (1);
+		}
+	}
+	new = add_node(key, value);
+	new->next = ht->array[index];
+	ht->array[index] = new;
+	return (1);
+}
+/**
+ *add_node - Write a function that adds an element to the hash table.
+ *@key: a variable
+ *@value: a variable
+ *Return: 0
+**/
+hash_node_t *add_node(const char *key, const char *value)
+{
+	hash_node_t *new;
+
 	new = malloc(sizeof(hash_table_t *));
 	if (new == NULL)
 		return (0);
@@ -28,7 +53,5 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new->value = strdup(value);
 	if (new->value == NULL)
 		return (0);
-	new->next = ht->array[index];
-	ht->array[index] = new;
-	return (1);
+	return (new);
 }
--- 0x1A-hash_tables/3-main.c
+++ 0x1A-hash_tables/3-main.c
@@ -13,6 +13,7 @@ int main(void)
 	hash_table_t *ht;
 
 	ht = hash_table_create(1024);
-	hash_table_set(ht, "betty", "holberton");
+	hash_table_set(ht, "hetairas", "mentioner");
+	hash_table_set(ht, "mentioner", "holberton");
 	return (EXIT_SUCCESS);
 }
