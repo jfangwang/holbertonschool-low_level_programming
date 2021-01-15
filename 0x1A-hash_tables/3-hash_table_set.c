d9c84c11b980704685579654c6e58886848a542b
jfangwang <qbs1864@gmail.com>
2021-01-15 20:44:14 +0000

    asdf

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -26,6 +26,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (ht->array[index] == NULL)
 	{
 		ht->array[index] = new;
+		return (1);
 	}
 	/**
 	 * Replace Value if key exists already
@@ -34,9 +35,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		if (strcmp(find->key, key) == 0)
 		{
+			free(find->value);
 			find->value = strdup(value);
-			return (1);
+			if (find->value == NULL)
+				return (0);
 		}
+		return (1);
 	}
 	new->next = ht->array[index];
 	ht->array[index] = new;
