157220d518a114739ae5a4afeacbf1e6072acb3a
jfangwang <qbs1864@gmail.com>
2021-01-15 21:39:41 +0000

    asdf

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -23,7 +23,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	 * Replace Value if key exists already
 	*/
 	find = ht->array[index];
-	while(find != NULL)
+	while (find != NULL)
 	{
 		if (!strcmp(find->key, key))
 		{
