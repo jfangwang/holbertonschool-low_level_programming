66c7419aa01ab15f145640c19f7815f242147a1e
jfangwang <qbs1864@gmail.com>
2021-01-15 18:40:32 +0000

    betty error test

--- 0x1A-hash_tables/3-main.c
+++ 0x1A-hash_tables/3-main.c
@@ -13,7 +13,13 @@ int main(void)
 	hash_table_t *ht;
 
 	ht = hash_table_create(1024);
-	hash_table_set(ht, "hetairas", "mentioner");
-	hash_table_set(ht, "mentioner", "holberton");
+	hash_table_set(ht, "c", "fun");
+	hash_table_set(ht, "python", "awesome");
+	hash_table_set(ht, "Bob", "and Kris love asm");
+	hash_table_set(ht, "N", "queens");
+	hash_table_set(ht, "Asterix", "Obelix");
+	hash_table_set(ht, "Betty", "Holberton");
+	hash_table_set(ht, "98", "Battery Street");
+	hash_table_print(ht);
 	return (EXIT_SUCCESS);
 }
