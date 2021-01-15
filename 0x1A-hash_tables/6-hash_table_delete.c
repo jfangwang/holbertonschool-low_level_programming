7dac870a9893f0ef8cbcd273367590b13605849e
jfangwang <qbs1864@gmail.com>
2021-01-15 20:23:53 +0000

    asdf

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -14,11 +14,11 @@ void hash_table_delete(hash_table_t *ht)
 	unsigned long int index;
 	hash_node_t *find, *temp;
 
-	if (ht == NULL || ht->size == 0 || ht->array == NULL)
+	if (ht == NULL)
 		return;
 	for (index = 0; index < ht->size; index++)
 	{
-		if (ht-array[index] != NULL)
+		if (ht->array[index] != NULL)
 		{
 			for (find = ht->array[index]; find != NULL; find = find->next)
 			{
