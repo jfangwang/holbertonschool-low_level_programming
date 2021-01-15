af73c526412f7b38efb03a68eee4a114b6b3df23
jfangwang <qbs1864@gmail.com>
2021-01-15 20:14:27 +0000

    asdf

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -37,7 +37,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		if (strcmp(find->key, key) == 0)
 		{
-			free(find->value);
 			find->value = strdup(value);
 			return (1);
 		}
