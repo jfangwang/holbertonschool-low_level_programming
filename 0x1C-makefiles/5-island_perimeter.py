72f02b35f22eec9b9416be704590de223ccc625b
jfangwang <qbs1864@gmail.com>
2021-02-19 22:49:02 +0000

    testing

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -14,6 +14,7 @@ def island_perimeter(grid):
     col = 0
     perim = 0
     add_2 = True
+    rcount = 0
     """horizontal"""
     for row in grid:
         col = 0
@@ -22,11 +23,12 @@ def island_perimeter(grid):
             b = col
             if row[b] == 1 and (b == 0 or b == len(row) - 1):
                 """edge cases"""
-                perim += 1
+                perim += 2
             elif row[b] == 1:
-                while row[b] == 1:
+                while row[b] == 1 and b < len(row):
                     b += 1
-                if add_2 == True:
+                    print(str(grid[rcount][b - 1]) + " perim " + str(perim), end = " ")
+                if add_2:
                     perim += 2
                     add_2 = False
                 else:
@@ -35,19 +37,23 @@ def island_perimeter(grid):
                 col = b
             else:
                 col += 1
-    col = 0
+            print(str(grid[rcount][col - 1]) + " perim " + str(perim), end = " ")
+        print()
+        rcount += 1
     row = 0
     for col in range(0, len(grid[row])):
         row = 0
         add_2 = True
-        while row < len(grid):
+        while row < len(grid) - 1:
             b = row
-            if grid[row][col] == 1 and (b == 0 or b == len(grid) - 1):
-                perim += 0
+            if grid[row][col] == 1 and (b == 0 or b == len(grid)):
+                perim += 1
+                add_2 = False
             elif grid[row][col] == 1:
-                while grid[b][col] == 1:
+                while grid[b][col] == 1 and b < len(grid) - 1:
                     b += 1
-                if add_2 == True:
+                    # print(str(grid[b - 1][col]) + " perim " + str(perim), end = " ")
+                if add_2 and b != len(grid) - 1:
                     perim += 2
                     add_2 = False
                 else:
@@ -56,4 +62,6 @@ def island_perimeter(grid):
                 row = b
             else:
                 row += 1
+            # print(str(grid[row - 1][col]) + " perim " + str(perim), end = " ")
+        print()
     return perim
