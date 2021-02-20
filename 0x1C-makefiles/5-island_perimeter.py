0299884ad4697a572e09fb6c73d8161a2bd3140b
jfangwang <qbs1864@gmail.com>
2021-02-20 04:59:23 +0000

    test

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -11,69 +11,40 @@ def island_perimeter(grid):
     # 0 1 0 1 0"""
     if grid is None:
         return 0
-    col = 0
     perim = 0
-    add_2 = True
-    rcount = 0
-    scout = 0
-    searching = False
     for row in range(0, len(grid)):
-        col = 0
-        add_2 = True
-        searching = False
-        while col < len(grid[row]):
-            scout = col
-            if grid[row][scout] == 1:
-                searching = True
-                while(grid[row][scout] == 1 and scout < len(grid[row]) - 1):
-                    scout += 1
-                searching = False
-                if add_2 == True:
-                    perim += 2
-                    add_2 = False
-                else:
+        for col in range(0, len(grid[row])):
+            print(grid[row][col], end=' ')
+            """outside row border"""
+            if grid[row][col] == 1:
+                if row < 0:
                     perim += 1
-            if scout > col:
-                col = scout
-            else:
-                col += 1
-        if searching:
-            if add_2:
-                perim += 2
-            else:
-                perim += 1
-        print()
-    col = 0
-    add_2 = True
-    rcount = 0
-    scout = 0
-    searching = False
-    for col in range(0, len(grid[0])):
-        row = 0
-        add_2 = True
-        searching = False
-        while row < len(grid) - 1:
-            scout = row
-            if grid[scout][col] == 1:
-                searching = True
-                while(grid[scout][col] == 1 and scout < len(grid) - 1):
-                    scout += 1
-                    print("a", end="")
-                searching = False
-                if add_2 == True:
-                    perim += 2
-                    add_2 = False
                 else:
+                    up = grid[row - 1][col]
+                if row + 1 >= len(grid):
                     perim += 1
-            if scout > row:
-                row = scout
-            else:
-                row += 1
-            print("a", end="")
-        if searching:
-            if add_2:
-                perim += 2
-            else:
-                perim += 1
+                else:
+                    down = grid[row + 1][col]
+                if col < 0:
+                    perim += 1
+                else:
+                    left = grid[row][col - 1]
+                if col + 1>= len(grid[row]):
+                    perim += 1
+                else:
+                    right = grid[row][col + 1]
+
+                if row >= 0 and up == 0:
+                    perim += 1
+                    up = 9
+                if row < len(grid) and down == 0:
+                    perim += 1
+                    down = 9
+                if col >= 0 and left == 0:
+                    perim += 1
+                    left = 9
+                if col < len(grid[row]) and right == 0:
+                    perim += 1
+                    right = 9
         print()
     return perim
