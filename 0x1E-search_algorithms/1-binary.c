903ed95b5dd990035b98e6c968fd16a4f5a6b228
jfangwang <qbs1864@gmail.com>
2021-04-20 18:26:44 +0000

    spacing

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -21,7 +21,7 @@ int binary_search(int *array, size_t size, int value)
 	while (beg <= end)
 	{
 		mid = (end + beg) / 2;
-		printf("Searching in array:");
+		printf("Searching in array: ");
 		for (a = beg; a < end; a++)
 			printf("%d, ", array[a]);
 		printf("%d\n", array[a]);
