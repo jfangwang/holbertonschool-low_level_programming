1ccdaedc1dd98a7289ae2560072c14a091299c7a
jfangwang <qbs1864@gmail.com>
2021-04-20 17:55:55 +0000

    added null array

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,6 +13,8 @@ int linear_search(int *array, size_t size, int value)
 {
 	int index = 0;
 
+	if (sizeof(array) == 0)
+		return (-1);
 	while (index < (int)size)
 	{
 		printf("Value checked array[%d] = [%d]\n", index, array[index]);
